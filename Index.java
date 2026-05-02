f714bbed774afe0a88e268e0d81dff414baaf069
Abhi <md.hussain@sunstone.in>
2026-05-02 15:20:21 +0530

    Final Commit

--- Index.java
+++ Index.java
@@ -1,1 +1,13 @@
-This is Git Lab Class
+class Index{
+public static void main(String args[])
+{
+int a=10,b=15;
+System.out.println("Before swap"+a,b);
+int temp;
+temp=a;
+a=b;
+b=temp;
+
+System.out.println("After swap"+a,b);
+}
+}
