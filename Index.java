5fcec1543af62b8d65bfba8b3d328233de2ee111
Abhi <md.hussain@sunstone.in>
2026-05-02 16:16:03 +0530

    v1

--- Index.java
+++ Index.java
@@ -1,13 +1,1 @@
-class Index{
-public static void main(String args[])
-{
-int a=10,b=15;
-System.out.println("Before swap"+a,b);
-int temp;
-temp=a;
-a=b;
-b=temp;
-
-System.out.println("After swap"+a,b);
-}
-}
+Now we r learning cclone and pull
